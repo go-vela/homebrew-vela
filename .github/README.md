c1b6dfaed9e076ea7d9f84a4d99a6b26c21e051a
Jordan Brockopp <Jordan.Brockopp@target.com>
2019-11-07 09:54:15 -0600

    initial commit

---
+++ .github/README.md
@@ -0,0 +1,15 @@
+# homebrew-vela
+
+This repository is the Tap for Vela [Homebrew](https://brew.sh/) formulas.
+
+This assumes that Homebrew is already installed and configured.
+
+## Installation
+
+```sh
+# add Vela tap to your brew configuration
+brew tap go-vela/vela
+
+# install Vela CLI
+brew install vela
+```
---
+++ Formula/vela@0.1.0.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class VelaAT010 < Formula
+  # repository information
+  head "https://github.com/go-vela/cli.git"
+  homepage 'https://github.com/go-vela/cli'
+
+  # utility information
+  version 'v0.1.0'
+  url "#{homepage}/releases/download/#{version}/vela_darwin_amd64.tar.gz"
+  sha256 '1e9e733d9626fe657cd7b13e4479e8f7f3bd3cda7acf9ddb9ff988610e0172a5'
+
+  # install information
+  def install
+    bin.install 'vela'
+  end
+
+  # test information
+  test do
+    system "#{bin}/vela", "--version"
+  end
+end
