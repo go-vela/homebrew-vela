e26cc79522d26b8e6efb2e07ad0db01f92b3405f
kneal <neal.coleman@target.com>
2020-08-12 14:59:11 -0500

    chore: update for release v0.5.0

---
+++ Formula/vela@0.5.rb
@@ -0,0 +1,26 @@
+# Copyright (c) 2019 Target Brands, Inc. All rights reserved.
+#
+# Use of this source code is governed by the LICENSE file in this repository.
+
+require 'formula'
+
+class VelaAT04 < Formula
+  # repository information
+  head "https://github.com/go-vela/cli.git"
+  homepage 'https://github.com/go-vela/cli'
+
+  # utility information
+  version 'v0.4.1'
+  url "#{homepage}/releases/download/#{version}/vela_darwin_amd64.tar.gz"
+  sha256 '2d6076f2f252ddbeb8605115c3cdc809d505fc0806286b1753c91964792e5bcb'
+
+  # install information
+  def install
+    bin.install 'vela'
+  end
+
+  # test information
+  test do
+    system "#{bin}/vela", "--version"
+  end
+end
