b98a3503604e1b069be84302bd82b124cf03df1f
David May <1301201+wass3r@users.noreply.github.com>
2021-10-24 13:17:11 +0000

    Update vela@0.10.rb

--- Formula/vela@0.10.rb
+++ Formula/vela@0.10.rb
@@ -12,8 +12,30 @@ class VelaAT09 < Formula
 
   # utility information
   version 'v0.10.2'
-  url "#{homepage}/releases/download/#{version}/vela_darwin_amd64.tar.gz"
-  sha256 '4c2d467769b61ed16e930c9ed036422a68e60cc5e406a112d9598f29036d42d1'
+  
+  # macOS
+  on_macos do
+    if Hardware::CPU.arm?
+      url "#{homepage}/releases/download/#{version}/vela_darwin_arm64.tar.gz"
+      sha256 '29c93600dd09975c30e67bc6134627f206fd543b6d1edfc397e9af090a818870'
+    else
+      url "#{homepage}/releases/download/#{version}/vela_darwin_amd64.tar.gz"
+      sha256 '4c2d467769b61ed16e930c9ed036422a68e60cc5e406a112d9598f29036d42d1'
+  end
+    
+  # linux
+  on_linux do
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "#{homepage}/releases/download/#{version}/vela_linux_arm64.tar.gz"
+        sha256 '5e793ec30155dbe29df3c6822018f6e812e243c219f0dc14371106044919eae4'
+      else
+        url "#{homepage}/releases/download/#{version}/vela_linux_arm.tar.gz"
+        sha256 '22d7a12b819a195f674b26d81fa4d415da0b63457fe0f4edb35b1a4523b07dd6'
+    else
+      url "#{homepage}/releases/download/#{version}/vela_linux_amd64.tar.gz"
+      sha256 '295b40efcf0dd3f602bd562734ea2657edbb4a1bd0a88a532bacab7e343a13ed'
+  end
 
   # install information
   def install
