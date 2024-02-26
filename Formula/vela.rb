34669c7d44b5422e0462ac5648c1c11892db8fbd
ecrupper <easton.crupper12@gmail.com>
2024-02-26 08:52:38 -0600

    chore(release): upgrade sha values to match v0.23.1 release

--- Formula/vela.rb
+++ Formula/vela.rb
@@ -8,16 +8,16 @@ class Vela < Formula
   homepage 'https://github.com/go-vela/cli'
 
   # utility information
-  version 'v0.23.0'
+  version 'v0.23.1'
 
   # macOS
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/#{version}/vela_darwin_arm64.tar.gz"
-      sha256 '8c6afd2bed0aded0117cd4999de95b1909463f881fee76265e650fb19400c9aa'
+      sha256 'b79769e9e4a6e527dd8ec4c9ebac6c0cf9672a497da99a1e1658a6fbd026b345'
     else
       url "#{homepage}/releases/download/#{version}/vela_darwin_amd64.tar.gz"
-      sha256 'd8f499a5ec2801812cddd4bd47b44a9156d807d4064187edbd15333184c532a3'
+      sha256 '74004b396a6dff3ad16956d73dce6a7a092cb96ebac9fb8919f8636a1af09ec2'
     end
   end
 
@@ -26,14 +26,14 @@ class Vela < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "#{homepage}/releases/download/#{version}/vela_linux_arm64.tar.gz"
-        sha256 '04f574af60ac463cf7939b48cd1c0afd646a3879184a509bb2a6af23fe4e54e5'
+        sha256 '7f2c610ae1ae4517df06d4d8c7d7ac65bbf459db2e39e1d353c5c6b140f22b29'
       else
         url "#{homepage}/releases/download/#{version}/vela_linux_arm.tar.gz"
-        sha256 '98321fa69518c06ba59ec00a347660b781e8cf39b853402b47a4076f43e1ae68'
+        sha256 '862e5280243c27c767ec3b99e54c1ca734a88f37504671910c7d4022bd77d262'
       end
     else
       url "#{homepage}/releases/download/#{version}/vela_linux_amd64.tar.gz"
-      sha256 '90fb6919750e7909a8de2949f7b5a4a6ab688a4b5491ea8332b7d23a7371464f'
+      sha256 '35da31056ce1fce1ed24af6ca59883ddc59ee4608b276f36e757fb6e9adc456a'
     end
   end
 
