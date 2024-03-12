3f3c439677a11612b95fb6e276996884c94d96ce
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2024-03-12 16:23:17 -0400

    chore(release): update checksums for v0.23.2 (#72)

--- Formula/vela@0.23.rb
+++ Formula/vela@0.23.rb
@@ -10,16 +10,16 @@ class VelaAT023 < Formula
   homepage 'https://github.com/go-vela/cli'
 
   # utility information
-  version 'v0.23.1'
+  version 'v0.23.2'
 
   # macOS
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/#{version}/vela_darwin_arm64.tar.gz"
-      sha256 'b79769e9e4a6e527dd8ec4c9ebac6c0cf9672a497da99a1e1658a6fbd026b345'
+      sha256 '392f4bedacf23b549c7b1bab85b2149b3b61db9ca4b613bfcbc058931d0b3dec'
     else
       url "#{homepage}/releases/download/#{version}/vela_darwin_amd64.tar.gz"
-      sha256 '74004b396a6dff3ad16956d73dce6a7a092cb96ebac9fb8919f8636a1af09ec2'
+      sha256 'd5fbf5a6455103898981c239f983c48279f56217c9a73a58e0f2c6b39cfd05f8'
     end
   end
 
@@ -28,14 +28,14 @@ class VelaAT023 < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "#{homepage}/releases/download/#{version}/vela_linux_arm64.tar.gz"
-        sha256 '7f2c610ae1ae4517df06d4d8c7d7ac65bbf459db2e39e1d353c5c6b140f22b29'
+        sha256 '946a301bd93fbfe79247358aa0b014f01bde93b31982078bb73889068654e64a'
       else
         url "#{homepage}/releases/download/#{version}/vela_linux_arm.tar.gz"
-        sha256 '862e5280243c27c767ec3b99e54c1ca734a88f37504671910c7d4022bd77d262'
+        sha256 '6af414c8fb1862beef4f4f48a2ab5f620c4a1a3ac401fc998d4b85db141f357d'
       end
     else
       url "#{homepage}/releases/download/#{version}/vela_linux_amd64.tar.gz"
-      sha256 '35da31056ce1fce1ed24af6ca59883ddc59ee4608b276f36e757fb6e9adc456a'
+      sha256 '920ac2a122b3c7c2646ed14c476a11660ca7c6e4244ea5bfe473b15806d125e5'
     end
   end
 
